b404a79f332f5ca76e12de13bff9f8bdc291427b
NoamHandelman <n.handelman6@gmail.com>
2023-12-21 06:50:30 +0200

    check after change next config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/v1/:path*',
-  //       destination: 'http://localhost:8080/api/v1/:path*',
-  //     },
-  //   ];
-  // },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.IMAGES_HOST,
+        hostname: 'd38sh0xhlkw1p8.cloudfront.net',
       },
     ],
   },
